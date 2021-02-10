a8450e5493e837f2ca093ed5c970ab7f8bb0fcaa
Josep <jfayos@cifpfbmoll.eu>
2021-02-10 17:01:48 +0100

    Actualizar proyecto
    Se actualiza el proyecto para que el test funcione

--- Project/test/com/jaume/penjat/TaulerTest.java
+++ Project/test/com/jaume/penjat/TaulerTest.java
@@ -1,4 +1,9 @@
-package test;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jaume.penjat;
 
 import com.jaume.penjat.Tauler;
 import org.junit.jupiter.api.BeforeEach;
